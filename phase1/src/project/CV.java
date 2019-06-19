1315a2d4dea192dbff674afa78b43669f397ca5d
Guikang Zhong <guikang.zhong@mail.utoronto.ca>
2019-06-19 15:39:15 -0400

    Modified CoverLetter and CV constructor using factory method

--- phase1/src/project/CV.java
+++ phase1/src/project/CV.java
@@ -1,5 +1,7 @@
 package project;
 
+import java.io.*;
+
 public class CV {
     public String getApplicantName() {
         return applicantName;
@@ -8,7 +10,29 @@ public class CV {
     private String applicantName;
     private String content;
 
-    public CV(String applicantName, String content){
+    public static CV createCVByDirectInput(String applicantName, String input){
+        return new CV(applicantName, input);
+    }
+
+    public static CV createCVByFileName(String applicantName, String path) throws IOException {
+        BufferedReader in = null;
+        String content = "";
+        try {
+            in = new BufferedReader(new FileReader(path));
+            String s = in.readLine();
+            while(s  != null){
+                content  = content + s;
+                s = in.readLine();
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+        return new CV(applicantName, content);
+    }
+
+    private CV(String applicantName, String content){
         this.applicantName = applicantName;
         this.content = content;
     }
--- phase1/src/project/CoverLetter.java
+++ phase1/src/project/CoverLetter.java
@@ -1,4 +1,5 @@
 package project;
+import java.io.*;
 
 public class CoverLetter {
     public String getApplicantName() {
@@ -8,7 +9,29 @@ public class CoverLetter {
     private String applicantName;
     private String content;
 
-    public CoverLetter(String applicantName, String content){
+    public static CoverLetter createCoverLetterByDirectInput(String applicantName, String input){
+        return new CoverLetter(applicantName, input);
+    }
+
+    public static CoverLetter createCoverLetterByFileName(String applicantName, String path) throws IOException{
+        BufferedReader in = null;
+        String content = "";
+        try {
+            in = new BufferedReader(new FileReader(path));
+            String s = in.readLine();
+            while(s  != null){
+                content  = content + s;
+                s = in.readLine();
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+        return new CoverLetter(applicantName, content);
+    }
+
+    private CoverLetter(String applicantName, String content){
         this.applicantName = applicantName;
         this.content = content;
     }
