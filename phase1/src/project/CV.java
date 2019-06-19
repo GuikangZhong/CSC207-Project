59d22e4396761402d6907fab473715bc4367601e
Guikang Zhong <guikang.zhong@mail.utoronto.ca>
2019-06-19 15:08:32 -0400

    Finished CoverLetter and CV

--- phase1/src/project/CV.java
+++ phase1/src/project/CV.java
@@ -1,4 +1,19 @@
 package project;
 
 public class CV {
+    private String applicantName;
+    private String content;
+
+    public CV(String applicantName, String content){
+        this.applicantName = applicantName;
+        this.content = content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
