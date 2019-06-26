f5893362868285136e49e0593fafb7c69300aa68
Guikang Zhong <guikang.zhong@mail.utoronto.ca>
2019-06-26 14:30:26 -0400

    Added addJobPosting and removeJobPosting

--- phase1/src/project/application/Document.java
+++ phase1/src/project/application/Document.java
@@ -1,6 +1,9 @@
 package project.application;
 
+import java.time.LocalDateTime;
+
 public abstract class Document {
     protected String applicantName;
     protected String content;
+    protected LocalDateTime createdDate;
 }
