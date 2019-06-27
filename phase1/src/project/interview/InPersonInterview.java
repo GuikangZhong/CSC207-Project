bae71bf5197495a2d35435bd1997f6ddc038d5af
Xiaochun Tong <xiaochun.tong@mail.utoronto.ca>
2019-06-27 15:16:05 -0400

    rewrite PhoneInterview and InPersonInterview

--- phase1/src/project/interview/InPersonInterview.java
+++ phase1/src/project/interview/InPersonInterview.java
@@ -3,20 +3,9 @@ package project.interview;
 import java.util.List;
 
 public class InPersonInterview extends Interview{
-    private final int maxInterview;
-    private List<InterviewAssignment> assignments;
 
-    InPersonInterview(int maximumInterview) {
-        super(maximumInterview);
-        this.maxInterview = maximumInterview - 1;
-    }
-
-    public int getMaxInterview() {
-        return maxInterview;
-    }
-
-    void setAssignments(List<InterviewAssignment> assignments) {
-        this.assignments = assignments;
+    InPersonInterview() {
+        super(3);
     }
 
     @Override
--- phase1/src/project/interview/PhoneInterview.java
+++ phase1/src/project/interview/PhoneInterview.java
@@ -3,21 +3,10 @@ package project.interview;
 import java.util.List;
 
 public class PhoneInterview extends Interview{
-    private final int maxInterview;
-    private List<InterviewAssignment> assignments;
-
-    PhoneInterview(int maximumInterview) {
-        super(maximumInterview);
-        this.maxInterview = 0;
+    PhoneInterview() {
+        super(1);
     }
 
-    public int getMaxInterview() {
-        return maxInterview;
-    }
-
-    void setAssignments(List<InterviewAssignment> assignments) {
-        this.assignments = assignments;
-    }
 
     @Override
     public String getInterviewType() {
