ff14813d69766197cd82e4d88170f4e869537257
kenny <qibowang7@outlook.com>
2019-06-28 19:02:55 -0400

    ApplicationMgr() added clock

--- phase1/src/project/user/ApplicantManager.java
+++ phase1/src/project/user/ApplicantManager.java
@@ -1,11 +1,18 @@
 package project.user;
 
+import project.system.SystemClock;
+
 public class ApplicantManager extends UserManager<Applicant> {
     @Override
     Applicant createUser(String name, String password) {
         return new Applicant(null, name, password);
     }
+    private SystemClock clock;
 
+    public ApplicantManager(SystemClock clock) {
+    	this.clock = clock;
+    }
+    
     void checkExpiredDocument(){
 
     }
