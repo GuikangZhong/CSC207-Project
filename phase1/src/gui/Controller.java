b28b0922e40b414d1d39b188d0f2967859593210
Guikang Zhong <guikang.zhong@mail.utoronto.ca>
2019-07-01 02:12:58 -0400

    Building GUI

---
+++ phase1/src/gui/Controller.java
@@ -0,0 +1,13 @@
+package gui;
+
+public class Controller {
+
+    public void loginButton(){
+        System.out.println("User logging in...");
+    }
+
+    public void signUpButton(){
+        System.out.println("User signing up...");
+    }
+
+}
---
+++ phase1/src/gui/Main.java
@@ -0,0 +1,31 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        primaryStage.setTitle("App");
+        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.show();
+    }
+
+    public void loginButton(){
+        System.out.println("User logging in...");
+    }
+
+    public void signUpButton(){
+        System.out.println("User signing up...");
+    }
+
+}
