0355538d19bcf4450edaa0ff93402c186eaa91d9
Guikang Zhong <guikang.zhong@mail.utoronto.ca>
2019-07-01 23:12:48 -0400

    Enabled to capture the input on GUI

--- phase1/src/gui/LoginController.java
+++ phase1/src/gui/LoginController.java
@@ -1,14 +1,22 @@
 package gui;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 import java.io.IOException;
 
 public class LoginController {
+
+    @FXML
+    private TextField usernameInput;
+    @FXML
+    private TextField passwordInput;
 
     public void returnButton(ActionEvent event) throws IOException {
         Parent main = FXMLLoader.load(getClass().getResource("Main.fxml"));
@@ -19,6 +27,8 @@ public class LoginController {
     }
 
     public void loginButton(ActionEvent event) throws IOException {
+        System.out.println(usernameInput.getText());
+        System.out.println(passwordInput.getText());
         Parent main = FXMLLoader.load(getClass().getResource("InterviewerMenu.fxml"));
         Scene mainScene = new Scene(main);
         Stage loginWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
--- phase1/src/gui/SignUpController.java
+++ phase1/src/gui/SignUpController.java
@@ -1,14 +1,21 @@
 package gui;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import java.io.IOException;
 
 public class SignUpController {
+
+    @FXML
+    private TextField usernameInput;
+    @FXML
+    private TextField passwordInput;
 
     public void returnButton(ActionEvent event) throws IOException {
         Parent main = FXMLLoader.load(getClass().getResource("Main.fxml"));
@@ -18,7 +25,7 @@ public class SignUpController {
         loginWindow.show();
     }
 
-    public void loginButton(ActionEvent event) throws IOException {
+    public void signUpButton(ActionEvent event) throws IOException {
         Parent main = FXMLLoader.load(getClass().getResource("Main.fxml"));
         Scene mainScene = new Scene(main);
         Stage loginWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
