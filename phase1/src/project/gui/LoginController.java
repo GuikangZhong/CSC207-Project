9583d84b0c5d572e90c687030ec66cd3b7b1c368
Guikang Zhong <guikang.zhong@mail.utoronto.ca>
2019-07-03 20:51:05 -0400

    Completed login and sign up

--- phase1/src/project/gui/LoginController.java
+++ phase1/src/project/gui/LoginController.java
@@ -32,32 +32,31 @@ public class LoginController {
 
     public void loginButton(ActionEvent event) throws IOException {
         User user = Main.system.login(usernameInput.getText(), passwordInput.getText());
-        SceneSwitcher.switchScene(this.getClass(), event, "ApplicantMenu.fxml");
-//        if (user == null){
-//            Stage window = new Stage();
-//
-//            //Block events to other windows
-//            window.initModality(Modality.APPLICATION_MODAL);
-//            window.setTitle("Warning");
-//            window.setHeight(100.0);
-//            window.setWidth(250.0);
-//
-//            Label label = new Label("User does not exist");
-//            Button closeButton = new Button("Close");
-//            closeButton.setOnAction(e -> window.close());
-//
-//            VBox layout = new VBox(10);
-//            layout.getChildren().addAll(label, closeButton);
-//            layout.setAlignment(Pos.CENTER);
-//
-//            //Display window and wait for it to be closed before returning
-//            Scene scene = new Scene(layout);
-//            window.setScene(scene);
-//            window.showAndWait();
-//        }
-//        else{
-//            SceneSwitcher.switchScene(this.getClass(), event, "ApplicantMenu.fxml");
-//        }
+        if (user == null){
+            Stage window = new Stage();
+
+            //Block events to other windows
+            window.initModality(Modality.APPLICATION_MODAL);
+            window.setTitle("Warning");
+            window.setHeight(100.0);
+            window.setWidth(250.0);
+
+            Label label = new Label("User does not exist");
+            Button closeButton = new Button("Close");
+            closeButton.setOnAction(e -> window.close());
+
+            VBox layout = new VBox(10);
+            layout.getChildren().addAll(label, closeButton);
+            layout.setAlignment(Pos.CENTER);
+
+            //Display window and wait for it to be closed before returning
+            Scene scene = new Scene(layout);
+            window.setScene(scene);
+            window.showAndWait();
+        }
+        else{
+            SceneSwitcher.switchScene(this.getClass(), event, "ApplicantMenu.fxml");
+        }
 
     }
 }
