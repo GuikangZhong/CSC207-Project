426d5f15168fcec39a6b675d6bd0e65179fc597d
Bolin Sun <bolin.sun@mail.utoronto.ca>
2019-07-06 12:29:41 -0400

    Added some comments related to some confusing minor issues about naming.

--- phase1/src/project/gui/HRMenuController.java
+++ phase1/src/project/gui/HRMenuController.java
@@ -28,8 +28,8 @@ public class HRMenuController implements Initializable {
         TreeItem<String> option = new TreeItem<>("Option");
         TreeItem<String> document = new TreeItem<>("View all applicants");
         TreeItem<String> jobPosting = new TreeItem<>("View applicants for a job posting");
-        TreeItem<String> application = new TreeItem<>("Inspect interviewers");
-        TreeItem<String> history = new TreeItem<>("Interview status");
+        TreeItem<String> application = new TreeItem<>("Inspect interviewers"); // Maybe change the name in the future.....
+        TreeItem<String> history = new TreeItem<>("Interview status");  // Handout P3 Paragraph 2
         companyName.setText(Main.user.getCompany());
 
         option.getChildren().addAll(document, jobPosting, application, history);
@@ -47,6 +47,17 @@ public class HRMenuController implements Initializable {
 //            loginWindow.setScene(mainScene);
 //            loginWindow.show();
 //        }
+        if (item != null){
+            if (item.getValue().equals("View all applicants")){
+                SceneSwitcher.switchScene(this.getClass(), event, "AllApplicants.fxml");
+            }
+            if (item.getValue().equals("View applicants for a job posting")){
+                SceneSwitcher.switchScene(this.getClass(), event, "PostingApplicants.fxml");
+            }
+            if (item.getValue().equals("Inspect interviewers")){
+                SceneSwitcher.switchScene(this.getClass(), event, "Miscellaneous.fxml");
+            }
+        }
     }
 
     public void exit(Event event) throws IOException{
