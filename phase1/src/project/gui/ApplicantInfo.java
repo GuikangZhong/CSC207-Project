31bb237b4d038698e7178fb1baa3f370c3fdb5ae
Bolin Sun <bolin.sun@mail.utoronto.ca>
2019-07-06 17:26:02 -0400

    Added a new class: ApplicantInfo where HR managers can get access to the information of the selected applicant.

---
+++ phase1/src/project/gui/ApplicantInfo.java
@@ -0,0 +1,20 @@
+package project.gui;
+
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import project.user.Applicant;
+import project.user.ApplicantManager;
+
+public class ApplicantInfo {
+    static Applicant applicant;
+
+    public void display(String applicantUsername){
+        ApplicantManager applicantManager = Main.system.getApplicants();
+        applicant = applicantManager.getUser(applicantUsername);
+
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(applicant.getRealName());
+
+    }
+}
