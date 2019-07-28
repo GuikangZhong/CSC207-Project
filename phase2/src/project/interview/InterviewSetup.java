9a17c482304deb460e0af4b0cc23c56b3f18735c
Bolin Sun <bolin.sun@mail.utoronto.ca>
2019-07-28 19:34:49 -0400

    Added some documents.

--- phase2/src/project/interview/InterviewSetup.java
+++ phase2/src/project/interview/InterviewSetup.java
@@ -17,6 +17,12 @@ public class InterviewSetup implements Serializable, Cloneable {
     private List<Round> rounds;
     private Map<String, Integer> record;
 
+
+    /**
+     * When a new InterviewSetup is created, by default it serves the purpose of being a certain "template" and should
+     * not be used as a concrete setting for a certain interview.
+     * @return true if and only if it serves as a template.
+     */
     protected boolean isTemplate() {
         return isTemplate;
     }
@@ -39,6 +45,13 @@ public class InterviewSetup implements Serializable, Cloneable {
         setup.isTemplate = false;
         return setup;
     }
+
+    /**
+     * Creates a concrete setup for the job based on the "template format".
+     * @param job: the job that the program need to set the interview format for.
+     * @return The concrete setup of interview for job.
+     * @throws CloneNotSupportedException
+     */
 
     public InterviewSetup createSetupWithJob(Job job) throws CloneNotSupportedException{
         InterviewSetup setup = (InterviewSetup)this.clone();
@@ -76,6 +89,13 @@ public class InterviewSetup implements Serializable, Cloneable {
         return rounds.size();
     }
 
+
+    /**
+     * Only used for the purpose of checking whether a format of interview process exists for the company,
+     * Does NOT serve the purpose of equals().
+     * @param other: the interview format to be compared.
+     * @return true if and only if the type of each round is the same.
+     */
     public boolean equalSetup(InterviewSetup other){
         if (rounds.size() != other.getRounds().size()){
             return false;
