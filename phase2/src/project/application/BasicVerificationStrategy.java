f88acabbd6c3beb4f6f411b349c2618a503559c6
Bolin Sun <bolin.sun@mail.utoronto.ca>
2019-07-31 18:43:55 -0400

    Added some comments.

--- phase2/src/project/application/BasicVerificationStrategy.java
+++ phase2/src/project/application/BasicVerificationStrategy.java
@@ -6,6 +6,15 @@ package project.application;
 public class BasicVerificationStrategy implements VerificationStrategy {
     private static final long serialVersionUID = 7376917471657116219L;
 
+
+    /**
+     * This verification passes if and only if the application contains exactly one CV and one cover letter.
+     *
+     * In our design, it is impossible that the application contains two CV's or two cover letters since the maximum
+     * allowed number for both of the two documents are 1.
+     * @param application: The application being verified.
+     * @return
+     */
     @Override
     public boolean satisfies(Application application) {
         int cnt = 0;
